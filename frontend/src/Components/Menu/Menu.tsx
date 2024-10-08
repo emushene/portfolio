bf310dc4a733a2ccf18cd08cfccdebcd86715cdf
emushene <e.mushene@gmail.com>
2024-10-08 09:50:37 +0300

    fixed trhge menu

--- frontend/src/Components/Menu/Menu.tsx
+++ frontend/src/Components/Menu/Menu.tsx
@@ -3,32 +3,37 @@ import React, { useState } from 'react'
 import menu from './menu.png'
 import { Link } from 'react-router-dom'
 
-
-interface Props  {}
+interface Props {}
 
 const Menu = (props: Props) => {
     const [open, SetOpen] = useState(false);
 
-  return (
-    <div className='flex flex-row'>
-        <img alt='Richard-Mucene-DevOps-Engineer-Johannesburg'
-         src={menu} 
-         width={28} height={28} 
-         className='cursor-pointer'
-         onClick={() => SetOpen(prev=>!prev)}/>
-         { open && (
-          <div className='absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl  z-10'>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/resume/"}>Resume</Link>
-            <Link to={"/dashboard"}>Dashboard</Link>
-            <Link to={"/search"}>Search</Link>
-            <Link to={"/blog"}>Blog</Link>
-            <Link to={"/contact"}>Contact</Link>
-            <Link to={"/logout"}>Logout</Link>
-          </div>
-         )}
-    </div>
-  )
+    const handleLinkClick = () => {
+        SetOpen(false); // Closes the menu when a link is clicked
+    };
+
+    return (
+        <div className='flex flex-row'>
+            <img 
+                alt='Richard-Mucene-DevOps-Engineer-Johannesburg'
+                src={menu} 
+                width={28} height={28} 
+                className='cursor-pointer'
+                onClick={() => SetOpen(prev => !prev)} 
+            />
+            {open && (
+                <div className='absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10'>
+                    <Link to="/" onClick={handleLinkClick}>Home</Link>
+                    <Link to="/resume/" onClick={handleLinkClick}>Resume</Link>
+                    <Link to="/dashboard" onClick={handleLinkClick}>Dashboard</Link>
+                    <Link to="/search" onClick={handleLinkClick}>Search</Link>
+                    <Link to="/blog" onClick={handleLinkClick}>Blog</Link>
+                    <Link to="/contact" onClick={handleLinkClick}>Contact</Link>
+                    <Link to="/logout" onClick={handleLinkClick}>Logout</Link>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default Menu
+export default Menu;
